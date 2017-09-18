217d11409a78fcb045038f1c8b28507411dabc06
Flavia Penim <flaviapenim@gmail.com>
2017-09-18 10:27:03 +0100

    Add updateSubmittablesFromCertificatesTest.

--- src/test/java/progressmonitor/ProgressMonitorTest.java
+++ src/test/java/progressmonitor/ProgressMonitorTest.java
@@ -1,18 +1,30 @@
 package progressmonitor;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.subs.data.status.ProcessingStatusEnum;
+import uk.ac.ebi.subs.processing.ProcessingCertificate;
+import uk.ac.ebi.subs.processing.ProcessingCertificateEnvelope;
 import uk.ac.ebi.subs.progressmonitor.ProgressMonitorService;
 import uk.ac.ebi.subs.progressmonitor.SubmittablesBulkOperations;
 import uk.ac.ebi.subs.repository.config.SubmittableConfig;
+import uk.ac.ebi.subs.repository.model.ProcessingStatus;
 import uk.ac.ebi.subs.repository.model.StoredSubmittable;
 import uk.ac.ebi.subs.repository.processing.SupportingSampleRepository;
 import uk.ac.ebi.subs.repository.repos.status.ProcessingStatusRepository;
+import util.Helpers;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @EnableMongoRepositories(basePackageClasses = {
@@ -30,9 +42,38 @@ import uk.ac.ebi.subs.repository.repos.status.ProcessingStatusRepository;
 })
 public class ProgressMonitorTest {
 
+    private String submittableId = "1234-5678";
+
+    @Autowired
+    private ProcessingStatusRepository processingStatusRepository;
+
+    @Autowired
+    private ProgressMonitorService progressMonitorService;
+
     @Before
-    public void setUp() { }
+    public void setUp() throws Exception {
+        ProcessingStatus ps = Helpers.generateProcessingStatus(submittableId, ProcessingStatusEnum.Processing);
+        processingStatusRepository.insert(ps);
+    }
 
     @Test
-    public void updateSubmittablesFromCertificatesTest() { }
+    public void updateSubmittablesFromCertificatesTest() {
+        ProcessingCertificate pc = Helpers.generateProcessingCertificate(submittableId, ProcessingStatusEnum.Submitted);
+        ProcessingCertificateEnvelope pce = Helpers.generateProcessingCertificateEnvelope(Arrays.asList(pc));
+
+        progressMonitorService.updateSubmittablesFromCertificates(pce);
+
+        assertThat(
+                processingStatusRepository.findBySubmittableId(submittableId).getStatus(), equalTo(ProcessingStatusEnum.Submitted.name())
+        );
+
+        assertThat(
+                processingStatusRepository.findBySubmittableId(submittableId).getAccession(), equalTo("SAMEA12345")
+        );
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        processingStatusRepository.deleteAll();
+    }
 }
--- src/test/java/util/Helpers.java
+++ src/test/java/util/Helpers.java
@@ -1,39 +1,40 @@
 package util;
 
 
-import uk.ac.ebi.subs.data.component.Team;
-import uk.ac.ebi.subs.repository.model.Sample;
-import uk.ac.ebi.subs.repository.model.Submission;
+import uk.ac.ebi.subs.data.component.Archive;
+import uk.ac.ebi.subs.data.status.ProcessingStatusEnum;
+import uk.ac.ebi.subs.processing.ProcessingCertificate;
+import uk.ac.ebi.subs.processing.ProcessingCertificateEnvelope;
+import uk.ac.ebi.subs.repository.model.ProcessingStatus;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class Helpers {
 
-    public static List<Sample> generateTestSamples() {
-        List<Sample> samples = new ArrayList<>();
-        Sample sample2 = new Sample();
-        sample2.setAccession("S2");
-        sample2.setDescription("Test sample 2.");
-        sample2.setId(UUID.randomUUID().toString());
-        samples.add(sample2);
-
-        Sample sample3 = new Sample();
-        sample3.setAccession("S3");
-        sample3.setDescription("Test sample 3.");
-        sample3.setId(UUID.randomUUID().toString());
-        samples.add(sample3);
-
-        return samples;
+    public static ProcessingStatus generateProcessingStatus(String submittableId, ProcessingStatusEnum processingStatus) {
+        ProcessingStatus ps = new ProcessingStatus(processingStatus);
+        ps.setId(UUID.randomUUID().toString());
+        ps.setSubmittableId(submittableId);
+        ps.setArchive(Archive.BioSamples.name());
+        ps.setSubmissionId(UUID.randomUUID().toString());
+        ps.setStatus(processingStatus);
+        return ps;
     }
 
-    public static Submission generateTestSubmission() {
-        Submission sub = new Submission();
-        sub.setId(UUID.randomUUID().toString());
-        Team d = new Team();
-        d.setName("subs-test");
-        sub.setTeam(d);
-        return sub;
+    public static ProcessingCertificateEnvelope generateProcessingCertificateEnvelope(List<ProcessingCertificate> certificates) {
+        ProcessingCertificateEnvelope pce = new ProcessingCertificateEnvelope();
+        pce.setSubmissionId("test-submission-id");
+        pce.setProcessingCertificates(certificates);
+        return pce;
+    }
+
+    public static ProcessingCertificate generateProcessingCertificate(String submittableId, ProcessingStatusEnum processingStatus) {
+        ProcessingCertificate pc = new ProcessingCertificate();
+        pc.setSubmittableId(submittableId);
+        pc.setProcessingStatus(processingStatus);
+        pc.setArchive(Archive.BioSamples);
+        pc.setAccession("SAMEA12345");
+        return pc;
     }
 }
