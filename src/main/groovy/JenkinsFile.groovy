1f3260d6bde0e51aebe6f729386c5e765f91de55
Neil Goodgame <neilg@ebi.ac.uk>
2017-10-25 13:23:14 +0100

    Test of env.BRANCH_NAME jenkins variable

--- src/main/groovy/JenkinsFile.groovy
+++ src/main/groovy/JenkinsFile.groovy
@@ -2,11 +2,19 @@ node {
     def mvnHome
     stage('Preparation') { // for display purposes
         // Get some code from a GitHub repository
+        echo 'Pulling...' + env.BRANCH_NAME
         checkout scm
         gradleHome = tool 'gradle-3'
     }
     stage('Build') {
         // Run the gradle assemble
         sh "gradlew assemble"
+    }
+    stage('Build') {
+        // Run the gradle assemble
+        sh "gradlew assemble"
+    }
+    stage('Deploy') {
+        echo env.BRANCH_NAME
     }
 }
