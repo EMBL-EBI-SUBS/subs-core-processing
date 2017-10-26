5a430af0494e5dd91b87a5c510157c0c18b6507a
Neil Goodgame <neilg@ebi.ac.uk>
2017-10-26 09:49:40 +0100

    Added deploy stage

--- src/main/groovy/JenkinsFile.groovy
+++ src/main/groovy/JenkinsFile.groovy
@@ -1,8 +1,5 @@
 node {
-    def mvnHome
     stage('Preparation') { // for display purposes
-        // Get some code from a GitHub repository
-        echo 'Pulling...' + env.BRANCH_NAME
         checkout scm
         gradleHome = tool 'gradle-3'
     }
@@ -10,11 +7,11 @@ node {
         // Run the gradle assemble
         sh "gradlew assemble"
     }
-    stage('Build') {
-        // Run the gradle assemble
-        sh "gradlew assemble"
-    }
     stage('Deploy') {
-        echo env.BRANCH_NAME
+        if (env.BRANCH_NAME == 'dev') {
+            sh "gradlew -Penv=dev deployJar"
+        } else if (env.BRANCH_NAME == 'master') {
+            sh "gradlew -Penv=test deployJar"
+        }
     }
 }
