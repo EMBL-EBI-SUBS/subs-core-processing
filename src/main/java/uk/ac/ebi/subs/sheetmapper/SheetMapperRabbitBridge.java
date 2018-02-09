32a715e2dbc7ef952f46f46b6f5aa139b8611955
David Richardson <davidr@ebi.ac.uk>
2018-02-09 16:37:46 +0000

    bind mapping service into rabbit bridge

--- src/main/java/uk/ac/ebi/subs/sheetmapper/SheetMapperRabbitBridge.java
+++ src/main/java/uk/ac/ebi/subs/sheetmapper/SheetMapperRabbitBridge.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
-import uk.ac.ebi.subs.apisupport.ApiSupportService;
-import uk.ac.ebi.subs.repository.model.Submission;
 import uk.ac.ebi.subs.repository.model.sheets.Sheet;
 import uk.ac.ebi.subs.repository.repos.SheetRepository;
 
@@ -17,12 +14,12 @@ public class SheetMapperRabbitBridge {
     private static final Logger logger = LoggerFactory.getLogger(SheetMapperRabbitBridge.class);
 
     private RabbitMessagingTemplate rabbitMessagingTemplate;
-    private ApiSupportService apiSupportService;
+    private SheetMapperService sheetMapperService;
     private SheetRepository sheetRepository;
 
-    public SheetMapperRabbitBridge(RabbitMessagingTemplate rabbitMessagingTemplate, ApiSupportService apiSupportService, SheetRepository sheetRepository) {
+    public SheetMapperRabbitBridge(RabbitMessagingTemplate rabbitMessagingTemplate, SheetMapperService sheetMapperService, SheetRepository sheetRepository) {
         this.rabbitMessagingTemplate = rabbitMessagingTemplate;
-        this.apiSupportService = apiSupportService;
+        this.sheetMapperService = sheetMapperService;
         this.sheetRepository = sheetRepository;
     }
 
@@ -31,9 +28,14 @@ public class SheetMapperRabbitBridge {
 
         logger.info("sheet ready for loading {}", sheet.getId());
 
-        sheetRepository.delete(sheet);
-    }
+        sheetMapperService.mapSheet(sheet);
 
+        logger.info("sheet mapped", sheet.getId());
+
+        sheetRepository.delete(sheet);
+
+        logger.info("sheet deleted", sheet.getId());
+    }
 
 
 }
