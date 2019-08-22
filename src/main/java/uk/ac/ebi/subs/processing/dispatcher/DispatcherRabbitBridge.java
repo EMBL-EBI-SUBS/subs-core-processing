4227d566cf03d0f079703f189de0f727a76a68fa
Karoly Erdos <karoly@ebi.ac.uk>
2019-08-22 11:36:17 +0100

    Change from millis to nanos

--- src/main/java/uk/ac/ebi/subs/processing/dispatcher/DispatcherRabbitBridge.java
+++ src/main/java/uk/ac/ebi/subs/processing/dispatcher/DispatcherRabbitBridge.java
@@ -147,10 +147,10 @@ public class DispatcherRabbitBridge {
         long elapsedTimeHours = ChronoUnit.HOURS.between(submissionEnd, submissionStart);
         long elapsedTimeMinutes = ChronoUnit.MINUTES.between(submissionEnd, submissionStart);
         long elapsedTimeSeconds = ChronoUnit.SECONDS.between(submissionEnd, submissionStart);
-        long elapsedTimeMilliSeconds = ChronoUnit.MILLIS.between(submissionEnd, submissionStart);
+        long elapsedTimeNanos = ChronoUnit.NANOS.between(submissionEnd, submissionStart);
         logger.info("Submission took {} to send",
                 String.format("%02d:%02d:%02d:%02d",
-                    elapsedTimeHours, elapsedTimeMinutes, elapsedTimeSeconds, elapsedTimeMilliSeconds)
+                    elapsedTimeHours, elapsedTimeMinutes, elapsedTimeSeconds, elapsedTimeNanos)
         );
     }
 }
